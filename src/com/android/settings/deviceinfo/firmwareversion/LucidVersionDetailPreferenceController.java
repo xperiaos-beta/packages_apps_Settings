fb8d33870d61e32729432a5de70517ae03e846d1
Jyotiraditya <dreadnaught02@outlook.com>
2020-03-31 13:29:31 +0000

    Settings: Make Lucid version screen more reliable
    Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/LucidVersionDetailPreferenceController.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2020 Lucid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import androidx.annotation.VisibleForTesting;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class LucidVersionDetailPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    private static final String LUCID_VERSION_PROP = "ro.lucid.version";
+
+    public LucidVersionDetailPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String lucidVersion = SystemProperties.get(LUCID_VERSION_PROP,
+                mContext.getString(R.string.device_info_default));
+        return lucidVersion;
+    }
+}
